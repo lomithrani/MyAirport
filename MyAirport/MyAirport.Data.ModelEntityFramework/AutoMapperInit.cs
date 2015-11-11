67aa3b682b03fa7f868d2a1ebddb0b719dcf5f22
Louis Gentil <gentil@ece.fr>
2015-11-11 23:15:04 +0100

    #part 1 push

---
+++ MyAirport/MyAirport.Data.ModelEntityFramework/AutoMapperInit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MyAirport.Entities;
+
+namespace MyAirport.Data.ModelEntityFramework
+{
+    public static class AutoMapperInit
+    {
+        static AutoMapperInit()
+        {
+            SetVolToVolDefinition();
+            SetBaggageToBaggageDefinition();
+        }
+
+        public static void Initialize()
+        {
+            
+        }
+
+        private static void SetVolToVolDefinition()
+        {
+            Mapper.CreateMap<VOL, VolDefinition>()
+                .ForMember(dest => dest.CIE, opt => opt.MapFrom(source => source.COMPAGNIE1.NOM))
+                .ForMember(dest => dest.Date, opt => opt.MapFrom(source => source.DHC.Value))
+                .ForMember(dest => dest.Ligne, opt => opt.MapFrom(source => source.LIG))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(source => source.ID_VOL));
+        }
+
+        private static void SetBaggageToBaggageDefinition()
+        {
+            Mapper.CreateMap<BAGAGE, BagageDefinition>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(source => source.ID_BAGAGE))
+                .ForMember(dest => dest.CodeIATA, opt => opt.ResolveUsing<IataResolver>())
+                .ForMember(dest => dest.DateCreation, opt => opt.MapFrom(source => source.DATE_CREATION))
+                .ForMember(dest => dest.IdVol, opt => opt.MapFrom(source => source.VOL.ID_VOL));
+
+
+            Mapper.CreateMap<BagageDefinition, BAGAGE>()
+                .ForMember(dest => dest.ID_BAGAGE, opt => opt.MapFrom(source => source.Id))
+                .ForMember(dest => dest.CODE_IATA, opt => opt.MapFrom(source => source.CodeIATA.ToString()))
+                .ForMember(source => source.DATE_CREATION, opt => opt.MapFrom(source => source.DateCreation))
+                .ForMember(dest => dest.ID_VOL, opt => opt.MapFrom(source => source.IdVol));
+        }
+
+        public class IataResolver : ValueResolver<BAGAGE, long>
+        {
+            protected override long ResolveCore(BAGAGE source)
+            {
+                long temp;
+                long.TryParse(source.CODE_IATA, out temp);
+                return temp;
+            }
+        }
+    }
+}
---
+++ MyAirport/MyAirport.Data.ModelEntityFramework/ModelEntity.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using MyAirport.Entities;
+
+
+namespace MyAirport.Data.ModelEntityFramework
+{
+    public class ModelEntity : AbstractModel
+    {
+        public ModelEntity()
+        {
+            AutoMapperInit.Initialize();
+        }
+
+        public override VolDefinition GetVol(int idVol)
+        {
+            VolDefinition valueToReturn;
+            using (var db = new MyAirportEntities())
+            {
+                var vol = db.VOLs.Single(v => v.ID_VOL == idVol);
+                valueToReturn = Mapper.Map<VolDefinition>(vol);
+            }
+            return valueToReturn;
+        }
+
+        public override BagageDefinition GetBagage(int idBagage)
+        {
+            BagageDefinition valueToReturn;
+            using (var db = new MyAirportEntities())
+            {
+                var baggage = db.BAGAGEs.Single(b => b.ID_BAGAGE == idBagage);
+                valueToReturn = Mapper.Map<BagageDefinition>(baggage);
+            }
+            return valueToReturn;
+        }
+
+        public override BagageDefinition GetBagageWithIata(string iata)
+        {
+            BagageDefinition valueToReturn;
+            using (var db = new MyAirportEntities())
+            {
+                BAGAGE baggage = null;
+                try
+                {
+                    baggage = db.BAGAGEs.Single(b => b.CODE_IATA == iata);
+                }
+                catch (Exception)
+                {
+                    
+                    //to do throw exception
+                }
+
+                valueToReturn = Mapper.Map<BagageDefinition>(baggage);
+            }
+            return valueToReturn;
+        }
+
+
+        public override List<BagageDefinition> GetBagages(BagageCriteres criteres)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int SetBaggage(BagageDefinition baggage)
+        {
+            var returnValue = -1;
+            using (var db = new MyAirportEntities())
+            {
+                var baggageDal = Mapper.Map<BAGAGE>(baggage);
+
+                db.BAGAGEs.Add(baggageDal);
+
+                try
+                {
+                    if (db.SaveChanges() >= 0)
+                    {
+                        returnValue = baggage.Id;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // ignored
+                }
+            }
+            return returnValue;
+        }
+
+        public override List<VolDefinition> GetVols(VolCriteres criteres)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
